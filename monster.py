178b9180d815a0a367b74cc499ca8328ba2b92e5
kishanss <67321746+kishanss@users.noreply.github.com>
2020-06-23 19:45:56 +0530

    Update monster.py

--- monster.py
+++ monster.py
@@ -113,7 +113,7 @@ jalan("\033[1;93m   ┈┈┈┈┈┈┈┈▕╲▂▂╱▏┈┈┈┈┈┈
 print "\033[1;93m♡─────╱▔▔▔▔┊┊┊┊▔▔▔▔╲───────♡\033[1;96mLogin RanaAhil\033[1;95m♡╰──────────•◈•──────────╯♡"
 
 CorrectUsername = "monster"
-CorrectPassword = "sks"
+CorrectPassword =  ansu
 
 loop = 'true'
 while (loop == 'true'):
